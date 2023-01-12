2f4c321bb9d155913e298906da0ea954720230c0
Brian J. Strout <brianjstrout@gmail.com>
2023-01-12 14:25:11 -0500

    enable slide in with intersect observe for first images

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("connected");
+
+const images = document.querySelectorAll(".loadMe");
+imgOptions = {};
+
+const imageLooker = new IntersectionObserver((entries, imageLooker) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      imageLooker.unobserve(entry.target);
+    }
+  });
+}, imgOptions);
+
+images.forEach((image) => {
+  imageLooker.observe(image);
+});
