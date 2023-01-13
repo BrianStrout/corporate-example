2d081a04d0b08a451eee1342cfb978278290b137
Brian J. Strout <brianjstrout@gmail.com>
2023-01-12 20:43:04 -0500

    changed layout of pictures in culture, added quote. Add observers to update logo color. Made class for logo to shift right

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 console.log("connected");
-
 const images = document.querySelectorAll(".loadMe");
-imgOptions = {};
+const logo = document.getElementById("logo");
+imgOptions = {
+  //   rootMargin: "100px 0px 0px 0px",
+  threshold: 0.5,
+};
 
 const imageLooker = new IntersectionObserver((entries, imageLooker) => {
   entries.forEach((entry) => {
@@ -17,3 +20,50 @@ const imageLooker = new IntersectionObserver((entries, imageLooker) => {
 images.forEach((image) => {
   imageLooker.observe(image);
 });
+
+const contentBody = document.getElementById("body-shell");
+const bodyoptions = {
+  threshold: 0.1,
+};
+const bodyEnterWAtch = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      //   entry.target.classList.add("test");
+      logo.classList.toggle("invert");
+
+      // imageLooker.unobserve(entry.target);
+    }
+  });
+}, bodyoptions);
+
+bodyEnterWAtch.observe(contentBody);
+
+//   images.forEach((image) => {
+//     imageLooker.observe(image);
+//   });
+
+const culture = document.getElementById("culture");
+
+const obsEnterringCulture = new IntersectionObserver();
+
+const b1 = "images/abg-ce.webp";
+const b2 = "images/abg-hf.webp";
+const b3 = "images/abg-ho.webp";
+
+let image = document.getElementById("img-flipper");
+
+let currentPos = 0;
+let images2 = [b1, b2, b3];
+
+function volgendefoto() {
+  if (++currentPos >= images2.length) currentPos = 0;
+
+  image.src = images2[currentPos];
+}
+
+// brianSecondPage.src = b1;
+
+setInterval(volgendefoto, 2000);
+// const body = document.body;
